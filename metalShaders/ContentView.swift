2a3a69433d6bfb12c0d5057aab27c18783b799db
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-01-01 15:54:22 -0600

    Initial Commit

---
+++ metalShaders/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  metalShaders
+//
+//  Created by Justin Hold on 1/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ metalShaders/metalShadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  metalShadersApp.swift
+//  metalShaders
+//
+//  Created by Justin Hold on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct metalShadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
